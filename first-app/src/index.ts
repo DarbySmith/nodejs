9d108b0383bae8a4719329ecf645f4d840ba70a9
Darby Smith <newman.darby2@gmail.com>
2024-02-08 20:14:17 -0700

    install packages

--- first-app/src/index.ts
+++ first-app/src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello from typescript");
+import express from 'express';
+
+const app = express()
+
+interface Params {
+    a: number;
+    b: number;
+}
+
+type Params2 = {
+    a: number;
+    b: number;
+}
+
+const add = (a: number, b: number): number => {
+    return a + b
+}
+
+app.get('/', (req) => {
+    add(1,2)
+})
+
+app.listen(3001, () => {
+    console.log("started");
+});
